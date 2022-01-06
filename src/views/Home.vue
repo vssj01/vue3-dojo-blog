4a3bc527be9847b93c8beacab3723260c11a9d05
vssj01 <vssj01@gmail.com>
2022-01-06 11:03:55 +0530

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    Home
+    <p>Hello my name is {{name}} and age is {{age}}</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  setup() {
+    console.log('setup')
+    let name = 'mario'
+    let age = 30
+
+    return {name, age}
+  
+  }
+}
+</script>

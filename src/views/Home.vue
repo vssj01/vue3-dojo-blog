dd87e7a44a17e8f079b2c7a3e70d8b657e2d2571
vssj01 <vssj01@gmail.com>
2022-01-06 11:15:16 +0530

    Using refs as reactive values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,30 @@
   <div class="home">
     Home
     <p>Hello my name is {{name}} and age is {{age}}</p>
+    <button @click="handleClick">Click Me</button>
+    <button @click="age++">Add 1 to age</button>
+    <br>
+    <input type="text" v-model="name">
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import {ref} from 'vue'
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
-    let name = 'mario'
-    let age = 30
+    let name = ref('mario')
+    let age = ref(30)
 
-    return {name, age}
+    const handleClick = () =>{
+      name.value = 'luigi'
+      age.value = 40  
+    }
+
+    return {name, age, handleClick}
   
   }
 }
